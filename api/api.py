18760ca05e1b6adc688f10ea116c8a76fcf6195c
r0zbot <r0zbot@users.noreply.github.com>
2019-01-04 03:55:54 -0200

    added extra stats to API

--- api/api.py
+++ api/api.py
@@ -1,12 +1,34 @@
+import requests
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 import json
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/info/cpu')
-def cpuInfo():
-    out = {}
-    out["temperature"] = int(int(open("/sys/class/thermal/thermal_zone0/temp").read())/1000)
+@app.route('/info')
+def sysinfo():
+    meminfo = {}
+    out2 = ""
+    for line in open("/proc/meminfo").readlines():
+        key,value = line.split(":");
+        meminfo[key.strip()] = value.replace("kB","").strip()
+    out = {
+        "ip": {
+            "client": request.remote_addr,
+            "server": requests.get('http://wtfismyip.com/text').content.decode("utf-8").strip()
+        },
+        "temperature": int(int(open("/sys/class/thermal/thermal_zone0/temp").read())/1000),
+        "memory": {
+            "ram": {
+                "free": meminfo["MemAvailable"],
+                "total": meminfo["MemTotal"] 
+            },
+            "swap": {
+                "free": meminfo["SwapFree"],
+                "total": meminfo["SwapTotal"]
+            }
+        }
+    }
     return json.dumps(out)
